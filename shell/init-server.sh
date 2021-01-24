5206e8478062921a85366de0c029a816ce650b9a
rxxy <891841484@qq.com>
2021-01-24 18:54:14 +0800

    Update init-server.sh

--- shell/init-server.sh
+++ shell/init-server.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-read -p "输入y确认继续操作:"  i
-if [ "$i" != y ];then
-                exit
-fi
 
+# 预检
+preCheck(){
+    version=$(rpm -q centos-release)
+    echo "系统版本: $version"
+    if [[ "" != "$(echo $version | grep centos-release-8)" ]];then
+        echo "预检失败，不支持centos8"
+        exit
+    fi
+}
 
 
 #额外的磁盘初始化
@@ -14,5 +19,26 @@ initDisk(){
         mkfs -t ext4 /dev/sdb1
 }
 
+# 安装docker
+installDocker(){
+    yum install -y yum-utils device-mapper-persistent-data lvm2
+    yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+    yum makecache fast
+    yum -y install docker-ce
 
-initDisk
+    # service docker start
+
+}
+
+
+preCheck
+
+read -p "预检成功，请输入y确认继续操作:"  i
+if [ "$i" != y ];then
+                exit
+fi
+
+# initDisk
+installDocker
+
+
