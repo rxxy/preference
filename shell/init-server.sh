5e2122be1e926909e5d60c1fe69df238b08e4a54
rxxy <891841484@qq.com>
2021-12-23 17:36:08 +0800

    Update init-server.sh

--- shell/init-server.sh
+++ shell/init-server.sh
@@ -66,6 +66,15 @@ if [ "$i" != y ];then
                 exit
 fi
 
+
+cd /etc/yum.repos.d
+cp CentOS-Base.repo CentOS-Base.repo.bak
+wget -O CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-8.repo
+yum clean all
+yum makecache
+yum update -y
+
+
 # initDisk
 installDocker
 #installAcme
