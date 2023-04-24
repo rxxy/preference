2a33e4969891428d185b6a72613d551ebb2b7966
rxxy <891841484@qq.com>
2023-04-24 12:11:35 +0800

    Update init-server.sh

--- shell/init-server.sh
+++ shell/init-server.sh
@@ -31,8 +31,8 @@ installDocker(){
     yum -y install docker-ce
     systemctl enable docker
     
-    curl -L https://get.daocloud.io/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-#    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#    curl -L https://get.daocloud.io/docker/compose/releases/download/v2.17.3/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    curl -L "https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose	
 	
     mkdir -p /etc/docker
